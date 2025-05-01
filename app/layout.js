599440c1d82c1170ba28bab97fe52422fd21fea7
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-01 20:45:47 +0530

    merged

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
     return (
       <html lang="en">
         <body>
-          <div id="root">hello</div>
+          <div id="root">hello123</div>
         </body>
       </html>
     )
